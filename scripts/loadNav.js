14d9525e106a841b8268a873a9f9aaa5560946fc
zazezizozuzy <mail@zazezizozuzy.com>
2024-06-09 17:43:18 -0700

    Moved to nav script and added chant page

---
+++ scripts/loadNav.js
@@ -0,0 +1,31 @@
+var nav = `
+<nav>
+    <ul>
+        <li>
+            <a href="index.html">Home</a>
+        </li>
+        <li>
+            <a href="party.html">Primes</a>
+        </li>
+        <li>
+            <a href="npcs.html">Cutters</a>
+        </li>
+        <li>
+            <a href="info.html">Chant</a>
+        </li>
+        <li>
+            <a href="events.html">Happenings</a>
+        </li>
+        <!--<li>
+            <a href="groups.html">Factions</a>
+        </li>-->
+        <li>
+            <a href="places.html">Planes</a>
+        </li>
+        <!--<li>
+            <a href="items.html">Pieces</a>
+        </li>-->
+    </ul>
+</nav>`;
+
+document.body.insertAdjacentHTML("afterbegin", nav);
